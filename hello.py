1ce750906da99ca151b12aac7b88a17ebee8e801
William Spagnola <whs278@nyu.edu>
2021-06-20 09:59:42 -0400

    Edits python file

--- hello.py
+++ hello.py
@@ -1,5 +1,6 @@
 x_list = [1, 2, 4, 5, 6]
 y_list = [x + 1 for x in x_list]
+print("Practicing list comprehensions")
 print('Function: y = x+ 1')
 print("X: {}".format(x_list))
 print("Y: {}".format(y_list)) 
